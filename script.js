43e5518527e1f65fea4019e2ac0789404376d2f8
asharanjith <ashasvijayan@gmail.com>
2022-10-10 18:30:53 +0400

    Fix Linter Error

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-const burgerMenu=document.querySelector('#hamburger');
-const mobileMenu=document.querySelector('.mobile-menu');
+const burgerMenu = document.querySelector('#hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-function openMenuBox(){
-    burgerMenu.style.display='none';
-    mobileMenu.style.display='flex';
+function openMenuBox() {
+  burgerMenu.style.display = 'none';
+  mobileMenu.style.display = 'flex';
 }
 
-burgerMenu.addEventListener('click',openMenuBox);
+burgerMenu.addEventListener('click', openMenuBox);
 
-const closeMobileMenu=document.querySelector('.fa-xmark');
+const closeMobileMenu = document.querySelector('.fa-xmark');
 
-function closeMenuBox(){
-    burgerMenu.style.display='block';
-    mobileMenu.style.display='none';
+function closeMenuBox() {
+  burgerMenu.style.display = 'block';
+  mobileMenu.style.display = 'none';
 }
-closeMobileMenu.addEventListener('click',closeMenuBox);
+closeMobileMenu.addEventListener('click', closeMenuBox);
 
-const mainSection=document.querySelector('.main-section');
-const aboutSection=document.querySelector('.about-section');
-const contactSection=document.querySelector('.contact-section');
+const mainSection = document.querySelector('.main-section');
+const aboutSection = document.querySelector('.about-section');
+const contactSection = document.querySelector('.contact-section');
 
-function goToMainSection(){
-    closeMenuBox();
-    window.location='#introductionMain';
+function goToMainSection() {
+  closeMenuBox();
+  window.location = '#introductionMain';
 }
-mainSection.addEventListener('click',goToMainSection);
+mainSection.addEventListener('click', goToMainSection);
 
-function goToAboutSection(){
-    closeMenuBox();
-    window.location='#about-myself';
+function goToAboutSection() {
+  closeMenuBox();
+  window.location = '#about-myself';
 }
-aboutSection.addEventListener('click',goToAboutSection);
+aboutSection.addEventListener('click', goToAboutSection);
 
-function goToContactSection(){
-    closeMenuBox();
-    window.location='#contactMeID';
+function goToContactSection() {
+  closeMenuBox();
+  window.location = '#contactMeID';
 }
-contactSection.addEventListener('click',goToContactSection);
+contactSection.addEventListener('click', goToContactSection);
