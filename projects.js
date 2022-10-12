a3c922b142a53dc52ca59a422847a3e73951be8f
asharanjith <ashasvijayan@gmail.com>
2022-10-12 12:54:16 +0400

    Add html content to the project section in projects.js

--- projects.js
+++ projects.js
@@ -40,3 +40,64 @@ const projectDetails = [
         year: 2018,
             },
 ];
+const works= document.querySelector('.projects');
+
+for(let i=0; i<projectDetails.length; i++){
+    const work = document.createElement('div');
+    work.classList.add('project');
+    work.innerHTML = `
+    <div class="sample-project ">
+        <div class="project-header ">
+            <div class="image-holder">
+                <img src="${projectDetails[i].headerImgD}" alt="Project Sample Image" class="header-img desktop">
+                <img src="${projectDetails[i].headerImgM}" alt="Project Sample Image" class="header-img mobile">
+            </div>
+        </div>
+
+        <div class="project-content">
+            <div class="project-description">
+                <div class="title-container">
+                    <h2 class="description-title">${projectDetails[i].descriptionTitle}</h2>
+                </div>
+                <div class="subtitle-container">
+                    <ul class="subtitle-items">
+                        <li class="list-less">${projectDetails[i].subHead}</li>
+                        <li class="list">${projectDetails[i].job}</li>
+                        <li class="list">${projectDetails[i].year}</li>
+                    </ul>
+                </div>
+                <div class="article-container">
+
+                    <p class="description-item">${projectDetails[i].descriptionItem}</p>
+                </div>
+                <div class="tag-container">            
+                    <ul class="tag-item">
+                        <li class="tag-btn tag-list-item">${projectDetails[i].tagBtn[0]}</li>
+                        <li class="tag-btn tag-list-item">${projectDetails[i].tagBtn[1]}</li>
+                        <li class="tag-btn tag-list-item">${projectDetails[i].tagBtn[2]}</li>
+                        <li class="tag-btn tag-list-item fourth">${projectDetails[i].tagBtn[3]}</li>
+                        
+                    </ul>
+            </div>
+            <div class="buttom-container">
+                <button class="project-buttom" onclick="modalOpen(${i})">See Project</button>
+            </div>
+        </div>
+    </div>
+    `;
+    works.appendChild(work);
+}
+
+const cards= document.querySelectorAll('.sample-project');
+const removeitem= document.querySelectorAll('.fourth');
+for(let j=0; j<cards.length; j++){
+    if(j%2===1){
+        cards[j].classList.add('project-reverse');
+
+    }
+    else
+    {
+        cards[j].classList.add('project-normal');
+        removeitem[j].classList.add('nodisplay');
+    }
+}
