507e5a184a846fb2bc3d40489c098725b2645042
asharanjith <ashasvijayan@gmail.com>
2022-10-12 16:01:28 +0400

    Fix style issues in modal window

--- modals.js
+++ modals.js
@@ -4,7 +4,7 @@ const modals = [
     descriptionItem: 'lorem  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     tagBtn: ['Html', 'Css', 'Javascript', 'Github', 'Ruby', 'Bootstrap'],
     headerImgM: './Assets/SnapshootPortfolio.png',
-    headerImgD: './Desktop/SnapshootDesktop.svg',
+    headerImgD: './Desktop/SnapshootPortfoliofigma.svg',
     subHead: 'CANOPY',
     job: 'Back End Dev',
     year: 2015,
